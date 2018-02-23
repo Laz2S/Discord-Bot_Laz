1a07fda251c10e431f6061660062537932e532e0
Unknown <lazsoares@hotmail.com>
2018-02-23 18:00:14 -0300

    third commit

--- bot.js
+++ bot.js
@@ -21,62 +21,212 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		case 'comandos':
                 bot.sendMessage({
                     to: channelID,
-                    message: '\nComandos-CS\n1csnade\n---------------\nComandos-LoL\n1lolbuild\n1lolprofile\n---------------\nAleatorio\n1stream\n1falabot\n1ping\n1rage\n1say\n1random\n1giria'
+                    message: '\***Comandos-CS***\n1csnade\n---------------\n***Comandos-LoL***\n1lolbuild\n1lolprofile\n---------------\n***Aleatorio***\n1elogio\n1motivacao\n1carlos\n1key\nmadu\n1clima\n1stream\n1falabot\n1ping\n1rage\n1say\n1random\n1giria'
                 });
             break;
 		case 'falabot':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Fala tu '+ user
-                });
+		var botR = Math.floor((Math.random() * 5) + 1);
+		switch(botR){
+				case 1:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Fala tu '+user
+                		});
+			    break;
+				case 2:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'kk eae '+user
+                		});
+			    break;
+				case 3:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Voce vem sempre aqui '+user+' ?'
+                		});
+			    break;
+				case 4:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Vamos ser amigos '+user+' !'
+                		});
+			    break;
+				case 5:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Estou feliz em te ver aqui '+user
+                		});
+			    break;
+                }
+            break;
+		case 'elogio':
+		var botR = Math.floor((Math.random() * 1) + 1);
+		switch(botR){
+				case 1:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Voce eh lindo como um girassol'
+                		});
+			    break;
+                }
+            break;
+		case 'motivacao':
+		var botR = Math.floor((Math.random() * 6) + 1);
+		switch(botR){
+				case 1:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Voce vai conseguir'
+                		});
+			    break;
+				case 2:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Eu acredito no seu potencial'
+                		});
+			    break;
+				case 3:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Nao desista no primeiro erro'
+                		});
+			    break;
+				case 4:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Voce nao estah sozinho'
+                		});
+			    break;
+				case 5:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Estou aqui por voce'
+                		});
+			    break;
+				case 6:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Todos erram'
+                		});
+			    break;
+                }
+            break;
+		case 'carlos':
+			var argsbuild = message.substring(1).split(' ');
+			var cmdbuild = args[0];
+    	    		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Eu te amo Carlos'
+                	});
+
+            break;
+		case 'key':
+			var argsbuild = message.substring(1).split(' ');
+			var cmdbuild = args[0];
+    	    		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Voce ja pensou em um comando ?'
+                	});
+
+            break;
+		case 'madu':
+			var argsbuild = message.substring(1).split(' ');
+			var cmdbuild = args[0];
+    	    		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Fale mais frases ai tmj eh nois'
+                	});
+
             break;
 		case 'stream':
 			var argsbuild = message.substring(1).split(' ');
 			var cmdbuild = args[0];
+			if(cmdbuild==null)
+			{
+    	    		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Entre com o nome do streammer'
+			});
+			}else
+			{
     	    		bot.sendMessage({
                     		to: channelID,
                     		message: 'https://www.twitch.tv/'+cmdbuild
                 	});
-
+			}
             break;
 		case 'csnade':
 			var argsbuild = message.substring(1).split(' ');
 			var cmdbuild = args[0];
+			if(cmdbuild==null)
+			{
+    	    		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Entre com o nome do mapa'
+			});
+			}else
+			{
     	    		bot.sendMessage({
                     		to: channelID,
                     		message: 'http://www.thewarmup.net/map/'+cmdbuild
                 	});
+			}
 
             break;
 		case 'lolbuild':
 			var argsbuild = message.substring(1).split(' ');
 			var cmdbuild = args[0];
+			if(cmdbuild==null)
+			{
+    	    		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Entre com o nome do campeao'
+			});
+			}else
+			{
     	    		bot.sendMessage({
                     		to: channelID,
                     		message: 'http://champion.gg/champion/'+cmdbuild
                 	});
+			}
 
             break;
 		case 'lolprofile':
-			var argsbuild = message.substring(9).split(' ');
+			var argsbuild = message.substring(1).split(' ');
 			var cmdbuild = args[0];
+			if(cmdbuild==null)
+			{
+    	    		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Entre com o nome da conta'
+			});
+			}else
+			{
     	    		bot.sendMessage({
                     		to: channelID,
                     		message: 'http://br.op.gg/summoner/champions/userName='+cmdbuild
                 	});
+			}
 
             break;
-/*
-		default:
-		if(message.substring(0,2) instanceof Number)
-		{
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'teste'
-                });
-		}
+                case 'clima':
+		var argsbuild = message.substring(1).split(' ');
+		var cmdbuild = args[0];
+			if(cmdbuild==null)
+			{
+    	    		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Entre com o estado e nome da cidade\nExemplo sp/saopaulo'
+			});
+			}else
+			{
+    	    		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'http://tempo.clic.com.br/'+cmdbuild
+                	});
+			}                
+
             break;
-*/
                 case 'ping':
                 bot.sendMessage({
                     to: channelID,
@@ -84,17 +234,47 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
             break;
 	        case 'rage':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Sai daqui boboca'
-                });
+		var botR = Math.floor((Math.random() * 5) + 1);
+		switch(botR){
+				case 1:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Sai daqui boboca'
+                		});
+			    break;
+				case 2:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Cala boca, por favor'
+                		});
+			    break;
+				case 3:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Com todo respeito, voce eh chato pra caramba'
+                		});
+			    break;
+				case 4:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Voce eh um bobalhao'
+                		});
+			    break;
+				case 5:
+                		bot.sendMessage({
+                    		to: channelID,
+                    		message: 'Se retire por gentileza'
+                		});
+			    break;
+		}
             break;
             	case 'say':
     	    	const sayMessage = args.join(" ");
-    	    	bot.sendMessage({
-                    to: channelID,
-                    message: sayMessage
-                });
+    	    		bot.sendMessage({
+                    		to: channelID,
+                    		message: sayMessage
+                	});
+
 	    break;
 	        case 'random':
                 bot.sendMessage({
@@ -103,7 +283,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
             break;
                 case 'giria':
-		var botG = Math.floor((Math.random() * 11) + 1);
+		var botG = Math.floor((Math.random() * 12) + 1);
 		switch(botG){
 				case 1:
 				bot.sendMessage({
@@ -169,6 +349,12 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				bot.sendMessage({
                     			to: channelID,
                     			message: 'Abreize'
+               			});
+			break;
+				case 12:
+				bot.sendMessage({
+                    			to: channelID,
+                    			message: 'Suave na nave'
                			});
 			break;
 		}
